4aa4afb6fa7c0c6f2959d3f3abc84be1aec9426d
Di Jin <332548128@qq.com>
2018-11-14 00:46:05 -0600

    completed with CRUD

--- public/javascripts/vidzy.js
+++ public/javascripts/vidzy.js
@@ -10,6 +10,17 @@ function($routeProvider, $locationProvider) {
             templateUrl: 'partials/video-form.html',
             controller: 'AddVideoCtrl'
         })
+        
+         .when('/video/:id',
+        {
+        templateUrl: 'partials/video-edit.html',
+        controller: 'EditVideoCtrl'
+        })
+         .when('/video/delete/:id', 
+        {
+        templateUrl: 'partials/video-delete.html',
+        controller: 'DeleteVideoCtrl'
+        })
         .otherwise({
             redirectTo: '/'
         });
@@ -36,3 +47,33 @@ app.controller('AddVideoCtrl',
             });
         };
     });
+app.controller('EditVideoCtrl', ['$scope', '$resource', '$location', '$routeParams',
+    function($scope, $resource, $location, $routeParams){   
+        var Videos = $resource('/api/videos/:id', { id: '@_id' }, {
+            update: { method: 'PUT' }
+        });
+
+        Videos.get({ id: $routeParams.id }, function(video){
+            $scope.video = video;
+        });
+
+        $scope.edit = function(){
+            Videos.update($scope.video, function(){
+                $location.path('/');
+            });
+        }
+    }]);
+app.controller('DeleteVideoCtrl', ['$scope', '$resource', '$location', '$routeParams',
+    function($scope, $resource, $location, $routeParams){
+        var Videos = $resource('/api/videos/:id');
+
+        Videos.get({ id: $routeParams.id }, function(video){
+            $scope.video = video;
+        })
+
+        $scope.delete = function(){
+            Videos.delete({ id: $routeParams.id }, function(video){
+                $location.path('/');
+            });
+        }
+    }]);
